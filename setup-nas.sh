df580ec0a75c78da093af431688f80dd96bc52c0
histvan95 <histvan.95@hotmail.hu>
2021-05-01 10:32:06 +0200

    Initial commit

---
+++ setup-nas.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt upgrade
+
+# Transmission
+sudo apt install transmission-daemon
+sudo systemctl stop transmission-daemon
+sudo mkdir -p ~/.config/transmission-daemon/
+sudo ln -s /etc/transmission-daemon/settings.json ~/.config/transmission-daemon/
+sudo chown -R ubuntu:ubuntu ~/.config/transmission-daemon/
+
+# MiniDLNA
+sudo apt-get install minidlna
+
+# Apache2 + PHP
+sudo apt-get install apache2
+sudo apt-get install php libapache2-mod-php
+
+# Samba
+sudo apt-get install samba samba-common-bin
